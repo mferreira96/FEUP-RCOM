35b742005e7ce8145f1d356bfc9321d061cb1c3d
mferreira96 <pount@sapo.pt>
2016-10-18 20:39:48 +0100

    working on llopen

--- proj1/DataLink.c
+++ proj1/DataLink.c
@@ -2,12 +2,48 @@
 #include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <termios.h>
+#include <signal.h>
 
 #include "DataLink.h"
 
 
-int llopen(int port, int flagMode){
+int llopen(char *port, int flagMode){
+  int fd;
+  struct termios oldtio,newtio;
 
+  fd = open(port, O_RDWR | O_NOCTTY );
+  if (fd <0) {perror(port); exit(-1); }
+
+  if ( tcgetattr(fd,&oldtio) == -1) { /* save current port settings */
+    perror("tcgetattr");
+    exit(-1);
+  }
+
+  bzero(&newtio, sizeof(newtio));
+  newtio.c_cflag = BAUDRATE | CS8 | CLOCAL | CREAD;
+  newtio.c_iflag = IGNPAR;
+  newtio.c_oflag = 0;
+
+  /* set input mode (non-canonical, no echo,...) */
+  newtio.c_lflag = 0;
+
+  newtio.c_cc[VTIME]    = 0;   /* inter-character timer unused */
+  newtio.c_cc[VMIN]     = 1;   /* blocking read until 1 chars received */
+
+
+
+  tcflush(fd, TCIOFLUSH);
+
+  if ( tcsetattr(fd,TCSANOW,&newtio) == -1) {
+    perror("tcsetattr");
+    exit(-1);
+  }
+
+  printf("New termios structure set\n");
   return 0;
 }
 
--- proj1/DataLink.h
+++ proj1/DataLink.h
@@ -1,8 +1,11 @@
 #ifndef LINKDATA_H
 #define LINKDATA_H
 
+#define BAUDRATE   B38400
+#define TRANSMITER  0
+#define RECEIVER    1
 
-int llopen(int port, int flagMode);
+int llopen(char * port, int flagMode);
 
 int llwrite(int fd, char *buffer, int length);
 
