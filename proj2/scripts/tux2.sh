72a6e648609983b8ab1833761e9f2ee97615d5b4
teresa-Guilherme <francisco_c_gomes@hotmail.com>
2016-11-21 17:22:16 +0000

    Add files via upload
    os scripts até à exp 3 , depois é chinês pra mim

---
+++ proj2/scripts/tux2.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+ifconfig eth0 up
+ifconfig eth0 172.16.51.1/24
+route add -net 172.16.50.0/24 gw 172.16.51.253
---
+++ proj2/scripts/tux4.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+ifconfig eth0 up
+ifconfig eth0 172.16.50.254/24
+ifconfig eth1 up
+ifconfig eth1 172.16.51.253/24
