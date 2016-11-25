8782847d36b8913d743ecffba5d5d62438e2ca11
teresa-Guilherme <francisco_c_gomes@hotmail.com>
2016-11-25 20:41:56 +0000

    dvisao da url

--- proj2/src/url.c
+++ proj2/src/url.c
@@ -1,5 +1,12 @@
 #include "url.h"
 
+/*
+char str[80]="ftp://[up201305016:BoraVanessa@]129.56.53.1/urlpath";
+
+init(str,<url structure>);
+
+tem de ser passado exclusivamente neste tipo de estrutura char str[N]=..., caso contrário temos uma visita da segmentation fault
+*/
 
 // ftp://[<user>:<password>@]<host>/<url-path>
 void init(char * url_name, url * url){
@@ -12,5 +19,27 @@ void init(char * url_name, url * url){
     exit(1);
   }
 
- // e preciso dividir o url
+ const char a[2] = "[";
+	const char b[2] = ":";
+	const char c[2] = "@";
+	const char d[2] = "/";
+
+	strtok(url_name, a);
+	url->user = strtok(NULL, b);
+	url->password = strtok(NULL, c);
+	url->host = strtok(NULL, d);
+	url->host++;
+	url->url_patch = strtok(NULL, d);  //nao seria suposto ser url->path?
+  
+  /*
+  TESTE para "ftp://[up201305016:BoraVanessa@]129.56.53.1/urlpath"
+  
+  printf( " %s\n", url->user );
+printf( " %s\n", url->password);
+printf( " %s\n", url->host);
+printf( " %s\n",url->url_patch); 
+
+funcionou bem
+  
+  */
 }
