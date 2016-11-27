ec52874ee2ace00ae4b2d19a5eacf50cbd947ace
teresa-Guilherme <francisco_c_gomes@hotmail.com>
2016-11-27 03:20:59 +0000

    Update connection.c

--- proj2/src/connection.c
+++ proj2/src/connection.c
@@ -13,21 +13,23 @@ int get_ip(connection * connection, char * host	){
       exit(1);
   }
 
-  printf("Host name  : %s\n", h->h_name);
-  printf("IP Address : %s\n",inet_ntoa(*((struct in_addr *)h->h_addr)));
-
+  //printf("Host name  : %s\n", h->h_name);
+  connection->ip=inet_ntoa(*((struct in_addr *)h->h_addr));
+  connection->port=21;
+  
   return 0;
 }
 
 
-int connect(){
-
+int connect(connection * connection){
+  
+  int sockfd;
   struct	sockaddr_in server_addr;
 /*server address handling*/
   bzero((char*)&server_addr,sizeof(server_addr));
   server_addr.sin_family = AF_INET;
-  server_addr.sin_addr.s_addr = inet_addr(SERVER_ADDR);	/*32 bit Internet address network byte ordered*/
-  server_addr.sin_port = htons(SERVER_PORT);		/*server TCP port must be network byte ordered */
+  server_addr.sin_addr.s_addr = inet_addr(connection->ip);	/*32 bit Internet address network byte ordered*/
+  server_addr.sin_port = htons(connection->port);		/*server TCP port must be network byte ordered */
 
 /*open an TCP socket*/
 if ((sockfd = socket(AF_INET,SOCK_STREAM,0)) < 0) {
@@ -39,16 +41,47 @@ if ((sockfd = socket(AF_INET,SOCK_STREAM,0)) < 0) {
     perror("connect()");
     exit(0);
   }
+  
+  connection->fileDescriptor=sockfd;
+  
+  //falta a parte de confirmar com o servidor mandando mensagens
+  
   return 0;
 }
 
 
-int login_host(){
-
+int login_host(connection * connection, url * url){
+  char * user;
+  char * pass;
+  char response[4];
+  sprintf(user, "USER %s\n", url->user);
+  write(connection->fileDescriptor, user, strlen(user));
+  read(connection->fileDescriptor, response, 3);
+  if(strcmp(response,"331")!=0)
+    return -1;
+  bzero(response,4);
+  
+  sprintf(pass, "PASS %s\n", url->password);
+  write(connection->fileDescriptor, pass, strlen(pass));
+  read(connection->fileDescriptor, response, 3);
+  
+  if(strcmp(response,"230")!=0)
+    return -1;
+  
   return 0;
 }
 
-int passive(){
+int passive(connection * connection,connection * connection2){
+  char response[1000];
+  write(connection->fileDescriptor, "pasv\n", strlen("pasv\n"));
+  read(connection->fileDescriptor, response, 1000);
+  
+  int ip1, ip2, ip3, ip4, p1, p2;
+  sscanf(response, "227 Entering Passive Mode (%d,%d,%d,%d,%d,%d)", &ip1,&ip2, &ip3, &ip4, &p1, &p2);
+
+  sprintf(connection2->host, "%d.%d.%d.%d", ip1, ip2, ip3, ip4);
+
+  connection2->port = p1*256+p2;
 
   return 0;
 }
