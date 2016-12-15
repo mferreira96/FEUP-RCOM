686f4d3418e2656889d1964174a6004fdabc6baa
teresa-Guilherme <francisco_c_gomes@hotmail.com>
2016-12-15 18:32:29 +0000

    Update tux1.sh

--- proj2/scripts/tux1.sh
+++ proj2/scripts/tux1.sh
@@ -3,3 +3,5 @@
 ifconfig eth0 up
 ifconfig eth0 172.16.51.1/24
 route add -net 172.16.51.0/24 gw 172.16.50.254
+route add default gw 172.16.50.254
+printf "search lixa.fe.up.pt\nnameserver 172.16.1.1\n" > /etc/resolv.conf
