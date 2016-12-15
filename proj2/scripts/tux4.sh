d94e8ee25548993e2371d0f3195af94c049dfe72
teresa-Guilherme <francisco_c_gomes@hotmail.com>
2016-12-15 18:34:49 +0000

    Update tux4.sh

--- proj2/scripts/tux4.sh
+++ proj2/scripts/tux4.sh
@@ -4,3 +4,7 @@ ifconfig eth0 up
 ifconfig eth0 172.16.50.254/24
 ifconfig eth1 up
 ifconfig eth1 172.16.51.253/24
+route add default gw 172.16.51.254
+echo 1 > /proc/sys/net/ipv4/ip_forward
+echo 0 > /proc/sys/net/ipv4/icmp_echo_ignore_broadcasts
+printf "search lixa.fe.up.pt\nnameserver 172.16.1.1\n" > /etc/resolv.conf
